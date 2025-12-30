db6b8325a26398b40306e3152e30b353dd8197af
roey_averbuch <roey6922@gmail.com>
2025-12-30 11:35:42 +0200

    adding datainteractor skeltons funcs + fixing in the contact rout

--- app/data_interactor.py
+++ app/data_interactor.py
@@ -3,4 +3,11 @@ from models.contacts import *
 
 
 class DataInteractor:
-    pass
+    def get_all_contacts()-> list[dict]:
+        pass
+    def create_contact(contact_data: dict)-> str:
+        pass
+    def update_contact(id: str, contact_data: dict)-> bool:
+        pass
+    def delete_contact(id: str)-> bool:
+        pass
--- app/routes/contact.py
+++ app/routes/contact.py
@@ -10,22 +10,22 @@ router = APIRouter(prefix="/contacts", tags=["Contacts"])
 @router.get("/")
 def list_contacts():
     """Get all contacts"""
-    return dti.get_contacts()
+    return dti.get_all_contacts()
 
 
 @router.post("/")
 def create_contact(contact: Contact):
     """Create new contact"""
-    return dti.create_new_contact(contact)
+    return dti.create_contact(contact.model_dump())
 
 
 @router.put("/{id}")
-def update_contact(id: int, contact: ContactUpdate):
+def update_contact(id: str, contact: ContactUpdate.model_dump()):
     """Update existing contact"""
     return dti.update_contact(contact_id=id, contact=contact)
 
 
 @router.delete("/{id}")
-def del_contact(id: int):
+def delete_contact(id: str):
     """Delete contact"""
     return dti.del_contact(id)
